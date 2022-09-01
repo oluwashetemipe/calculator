b66f78bab14417f83ebf8668f8f499fb727581fb
Oluwashetemipe <oluwashetemipe@gmail.com>
2022-09-01 18:57:49 +0100

    updates

--- src/main/java/com/restful/calculator/model/Result.java
+++ src/main/java/com/restful/calculator/model/Result.java
@@ -10,7 +10,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Result {
 
-    private long id;
     private double result;
 
 
--- src/main/java/com/restful/calculator/service/CalculatorServiceImpl.java
+++ src/main/java/com/restful/calculator/service/CalculatorServiceImpl.java
@@ -2,32 +2,48 @@ package com.restful.calculator.service;
 
 import com.restful.calculator.model.Result;
 import com.restful.calculator.model.Values;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
-
+@Service
 public class CalculatorServiceImpl implements CalculatorService{
-    @Override
-    public Result add(Optional<Values> values) {
+    private Values extractValues(Optional<Values> values){
         if (values.isEmpty()){
-            throw new IllegalArgumentException("");
+            throw new IllegalArgumentException("Values cannot be empty");
         }
-        long first = values.get().getFirstOperand();
-        long second = values.get().getSecondOperand();
-        return new Result((first+second));
+        double first = values.get().getFirstOperand();
+        double second = values.get().getSecondOperand();
+        String operator = values.get().getOperator();
+        Values extractedValues = new Values();
+        extractedValues.setFirstOperand(first);
+        extractedValues.setSecondOperand(second);
+        extractedValues.setOperator(operator);
+        return extractedValues;
     }
-
     @Override
-    public Result multiply(Optional<Values> values) {
-        return null;
-    }
+    public Result calculate(Optional<Values> values) {
+        Values extractValues = extractValues(values);
+        Result result = new Result();
+        switch(extractValues.getOperator()) {
+            case "+":
+                result.setResult(extractValues.getFirstOperand() + extractValues.getSecondOperand());
+                break;
+            case "-":
+                result.setResult(extractValues.getFirstOperand() - extractValues.getSecondOperand());
+                break;
+            case "*":
+                result.setResult(extractValues.getFirstOperand() * extractValues.getSecondOperand());
+                break;
+            case "/":
+                result.setResult(extractValues.getFirstOperand() / extractValues.getSecondOperand());
+                break;
+            case "^":
+                result.setResult(Math.pow(extractValues.getFirstOperand(),extractValues.getSecondOperand()));
+                break;
+            default:
+                result.setResult(0);
+        }
 
-    @Override
-    public Result divide(Optional<Values> values) {
-        return null;
-    }
-
-    @Override
-    public Result subtract(Optional<Values> values) {
-        return null;
+        return result;
     }
 }
